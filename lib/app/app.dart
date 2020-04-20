92cd187df9622ccb8d01d18eeb32d72304b86a2a
Christian Hizon <christiangerardhizon@gmail.com>
2020-04-20 12:49:56 +0800

    Initial Packages

---
+++ lib/app/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Application extends StatefulWidget {
+  @override
+  _ApplicationState createState() => _ApplicationState();
+}
+
+class _ApplicationState extends State<Application> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'CovidStats',
+    );
+  }
+}
